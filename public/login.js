9a125903589764b29dd47994bea0c1d9ca955c31
sujal <sujal2september@gmail.com>
2025-04-18 12:47:06 +0545

    made login possible

---
+++ public/login.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('registration-form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form)
+    const data = Object.fromEntries(formData.entries())
+    console.log(data);
+    login(data)
+});
+
+function login(data){
+    fetch('http://localhost:3455/login',{
+        method : 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+        
+    })
+    .then(res => {
+        if(res.ok){
+            alert('login success')
+            window.location.href = "./notelist.html"
+        }
+        else {
+        alert('unable to login')
+        }
+    })
+}
+
